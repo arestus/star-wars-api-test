0c2160fe2b8e96a8c07e06edf40e061daee12010
arestus <arestus@gmail.com>
2021-05-17 11:03:26 +0300

    add styles for person page

---
+++ src/components/PersonPage/PersonPhoto/PersonPhoto.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+import styles from "./PersonPhoto.module.css";
+
+const PersonPhoto = ({ personPhoto, personName }) => {
+  return (
+    <>
+      <div className={styles.container}>
+        <img className={styles.photo} src={personPhoto} alt={personName} />
+      </div>
+    </>
+  );
+};
+
+PersonPhoto.propTypes = {
+  personPhoto: PropTypes.string,
+  personName: PropTypes.string,
+};
+
+export default PersonPhoto;
--- src/containers/PersonPage/PersonPage.js
+++ src/containers/PersonPage/PersonPage.js
@@ -1,23 +1,62 @@
+import PropTypes from "prop-types";
 import { getApiResource } from "@utils/network";
-import { useEffect } from "react";
+import { getPeopleImage } from "@services/getPeopleData";
+import PersonInfo from "@components/PersonPage/PersonInfo";
+import PersonPhoto from "@components/PersonPage/PersonPhoto";
+import { useEffect, useState } from "react";
 import { API_PERSON } from "@constants/api";
+import { withErrorApi } from "@hoc-helpers/withErrorApi";
 import styles from "./PersonPage.module.css";
 
-const PersonPage = ({ match }) => {
+const PersonPage = ({ match, setErrorApi }) => {
+  const [personInfo, setPersonInfo] = useState(null);
+  const [personName, setPersonName] = useState(null);
+  const [personPhoto, setPersonPhoto] = useState(null);
+
   useEffect(() => {
     (async () => {
       const id = match.params.id;
       const res = await getApiResource(`${API_PERSON}/${id}/`);
 
-      console.log(`${API_PERSON}/${id}/`, res);
+      if (res) {
+        setPersonInfo([
+          { title: "Height", data: res.height },
+          { title: "Mass", data: res.mass },
+          { title: "Hair Color", data: res.hair_color },
+          { title: "Sking Color", data: res.skin_color },
+          { title: "Eye Color", data: res.eye_color },
+          { title: "Birth Year", data: res.birth_year },
+          { title: "Gender", data: res.gender },
+        ]);
+        setPersonName(res.name);
+        setPersonPhoto(getPeopleImage(id));
+
+        //   res.films
+
+        setErrorApi(false);
+      } else {
+        setErrorApi(true);
+      }
     })();
   }, []);
 
   return (
     <>
-      <h1>PersonPage </h1>
+      <div className={styles.wrapper}>
+        <span className={styles.person__name}>{personName} </span>
+        <div className={styles.container}>
+          <PersonPhoto personPhoto={personPhoto} personName={personName} />
+
+          {personInfo && <PersonInfo personInfo={personInfo} />}
+        </div>
+      </div>
     </>
   );
 };
 
-export default PersonPage;
+PersonPage.propTypes = {
+  setErrorApi: PropTypes.func,
+  match: PropTypes.object,
+};
+
+export default withErrorApi(PersonPage);
