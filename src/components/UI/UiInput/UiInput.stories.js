73b410aae43573c4b3c33ce1c26684cf57f17847
arestus <arestus@gmail.com>
2021-05-29 12:24:47 +0300

    add uiinput

---
+++ src/components/UI/UiInput/UiInput.stories.js
@@ -0,0 +1,30 @@
+import UiInput from "./UiInput";
+import { useState } from "react";
+
+export default {
+  title: "Ui-Kit/UiInput",
+  component: UiInput,
+};
+
+const Template = (arg) => {
+  const [value, setValue] = useState("");
+
+  const handleInputChange = (value) => {
+    setValue(value);
+  };
+  return (
+    <UiInput {...arg} value={value} handleInputChange={handleInputChange} />
+  );
+};
+
+const props = {
+  value: "",
+  handleInputChange: () => console.log("Input Change"),
+  placeholder: "Placeholder",
+  classes: "",
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  ...props,
+};
