58dd75b1e343bfa4c7705da6a262b875a07dcd16
arestus <arestus@gmail.com>
2021-06-14 21:05:35 +0300

    add package settings

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import routesConfig from "@routes/routesConfig";
 import Header from "@components/Header";
+import { REPO_NAME } from "@constants/repo";
 
 import styles from "./App.module.css";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={`/${REPO_NAME}/`}>
         <div className={styles.wrapper}>
           <Header />
           <Switch>
